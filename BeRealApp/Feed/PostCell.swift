4671e8a208dfadfc753bff8914f60291b3ac5127
Anthony Rufin <114187227+arufi004@users.noreply.github.com>
2024-03-06 15:41:53 -0500

    Add files via upload

--- BeRealApp/Feed/PostCell.swift
+++ BeRealApp/Feed/PostCell.swift
@@ -16,9 +16,14 @@ class PostCell: UITableViewCell {
     
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var captionLabel: UILabel!
+
+
     @IBOutlet weak var postImageView: UIImageView!
+    
     private var imageDataRequest: DataRequest?
 
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
     func configure(with post: Post) {
         // TODO: Pt 1 - Configure Post Cell
         // Username
@@ -41,15 +46,37 @@ class PostCell: UITableViewCell {
                     break
                 }
             }
-        }
-
+        }       
+        
+        
         // Caption
         captionLabel.text = post.caption
-
         // Date
         if let date = post.createdAt {
             dateLabel.text = DateFormatter.postFormatter.string(from: date)
         }
+
+        // A lot of the following returns optional values so we'll unwrap them all together in one big `if let`
+        // Get the current user.
+        if let currentUser = User.current,
+
+            // Get the date the user last shared a post (cast to Date).
+           let lastPostedDate = currentUser.lastPostedDate,
+
+            // Get the date the given post was created.
+           let postCreatedDate = post.createdAt,
+
+            // Get the difference in hours between when the given post was created and the current user last posted.
+           let diffHours = Calendar.current.dateComponents([.hour], from: postCreatedDate, to: lastPostedDate).hour {
+
+            // Hide the blur view if the given post was created within 24 hours of the current user's last post. (before or after)
+            blurView.isHidden = abs(diffHours) < 24
+        } else {
+
+            // Default to blur if we can't get or compute the date's above for some reason.
+            blurView.isHidden = false
+        }
+
 
     }
 
